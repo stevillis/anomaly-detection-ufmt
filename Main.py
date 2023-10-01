1b4717c814720e0c6a251769a24ba7bec57f538e
Stévillis Sousa <stevillis@hotmail.com>
2023-10-01 18:31:00 -0400

    feat: create main page

--- Main.py
+++ Main.py
@@ -1,1 +1,9 @@
+import numpy as np
 import streamlit as st
+
+st.title("Pós Graduação em Gestão e Ciência de Dados - UFMT")
+st.header("Detecção de Anomalias")
+
+st.write(
+    "Use o **Menu Lateral** para acessar os Exercícios de Métodos de Detecção de Anomalias utilizando Python."
+)
